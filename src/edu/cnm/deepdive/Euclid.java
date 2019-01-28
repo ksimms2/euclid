54e53ae3891289a6792965de41032fc57ed98bb2
Kevin Simms <ksimms2@cnm.edu>
2019-01-28 16:17:45 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Euclid.java
@@ -0,0 +1,33 @@
+package edu.cnm.deepdive;
+
+public class Euclid {
+
+  public static long gcd(long a, long b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (a == 0) {
+      if (b == 0) {
+        throw new ArithmeticException("GCD(0 , 0) is undefined.");
+      }
+      return b;
+    }
+    if (b == 0) {
+      return a;
+    }
+    while (b != 0 && a != 0) {
+
+      if (a > b) {
+        a %= b;
+      } else {
+        b %= a;
+      }
+    }
+    if (a != 0) {
+      return a;
+    } else {
+      return b;
+    }
+
+  }
+
+}
